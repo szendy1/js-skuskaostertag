cbeabcbd184c2a0a8dd81cd7d6b54c47f3dfc960
szendy1 <szendy1@uniba.sk>
2019-05-21 09:40:16 +0200

    2 files modified

--- index.js
+++ index.js
@@ -3,8 +3,25 @@ import './style.css';
 
 
 let score = 0;
+let cWidth = 600;
+let cHeight = 400;
 
 
 // Write Javascript code!
-const appDiv = document.getElementById('app');
-appDiv.innerHTML = `<h1>JS Starter</h1>`;
+const canvas = document.getElementById('app');
+let ctx = canvas.getContext("2d");
+
+
+function paintBg(){
+  ctx.fillStyle = 'black';
+  ctx.fillRect(0, 0, cWidth, cHeight);
+  console.log("loading")
+}
+
+window.onload = function s(){
+  console.log("lslsls")
+  paintBg();
+}
+
+
+
