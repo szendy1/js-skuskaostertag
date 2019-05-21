6e520da9aaa7fc5d77e94ec6e22e564eac1e0f24
szendy1 <szendy1@uniba.sk>
2019-05-21 09:27:23 +0200

    Initial commit

---
+++ index.js
@@ -0,0 +1,10 @@
+// Import stylesheets
+import './style.css';
+
+
+let score = 0;
+
+
+// Write Javascript code!
+const appDiv = document.getElementById('app');
+appDiv.innerHTML = `<h1>JS Starter</h1>`;
